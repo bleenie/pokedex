0bf791b89159fc10cca6f898835db10b36dfc376
bleenie <117716131+bleenie@users.noreply.github.com>
2024-04-09 11:49:02 +0200

    setup

--- pokedex-project/src/App.jsx
+++ pokedex-project/src/App.jsx
@@ -3,30 +3,65 @@ import "./style.scss"
 
 
 export default function App() {
+  const [allPokemon, setAllPokemon] = useState();
   const [pokemonData, setPokemonData] = useState();
-  const [pokemonInfo, setPokemonInfo] = useState();
-
+  
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=1302")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
     .then((response) => response.json())
-    .then((json) => setPokemonData(json));
+    .then((json) => setAllPokemon(json));
   }, []);
 
-  useEffect(() => {
-    fetch(pokemonData?.results[0].url)
-    .then((response) => response.json())
-    .then((json) => setPokemonInfo(json));
-  }, []);
-
-  return <div className="pokemon">
-    {
-      <>
-    <p className="pokemon-name">
-      Pokemon: {pokemonData?.results[0].name}
-    </p>
-    <img src={pokemonInfo?.sprites.front_default} alt={pokemonData?.results[0].name}></img>
-    </>
-    }
-  </div>
+  return (
+    <>
+    {allPokemon?.results.map(pokemon => (
+      
+      <div key={pokemon.name} className="card">
+        <p>{pokemon.name}</p>
+        <img src="`https://img.pokemondb.net/sprites/black-white/normal/${pokemon.name}png`" alt={pokemon.name}></img>
+      </div>
+      ))}
+      </>
+  );
 }
 
+
+// {pokemonData.map(pokemon => (
+//   <div className="card">
+//     <p>{pokemon.name}</p>
+//     <img src={pokemon.sprites.front_default} alt={pokemon.name}></img>
+//   </div>
+//   ))}
+
+
+
+
+
+
+
+// useEffect(() => {
+//   fetch(pokemonData?.results[0].url)
+//   .then((response) => response.json())
+//   .then((json) => setPokemonInfo(json))
+//   .then 
+// }, []);
+
+
+
+
+// {pokemonData?.results.map(pokemon => (
+// <div key={pokemon.name}>
+//   <p>{pokemon.name}</p>
+//   <img src={pokemonInfo?.sprites.front_default} alt={pokemonData?.results[0].name}></img>
+// </div>
+// ))}
+
+// return (
+//   <div className="pokemon">
+//     <p className="pokemon-name">
+//       Pokemon: {pokemonData?.results[0].name}
+//     </p>
+//     <img src={pokemonInfo?.sprites.front_default} alt={pokemonData?.results[0].name}></img>
+    
+//   </div>
+//   );
